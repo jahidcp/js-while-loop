27096901b3032edec516b41f095c48e8df75decc
jahidcp <programmingloverjahid@gmail.com>
2023-01-28 19:26:11 +0600

    some while loop practice with js

---
+++ js_while_loop.js
@@ -0,0 +1,42 @@
+var token = 0;
+while(token < 10){
+    console.log("Every hunter Received some amount of token")
+    console.log(token)
+    token++;
+}
+
+
+var name = 0;
+while(name < 20){
+    console.log("Hello World!")
+    name+=1;
+}
+
+
+var propose = 0;
+while(propose < 5){
+    console.log("I love You Jan!")
+    propose += 1;
+}
+
+
+var num = 0;
+while(num <= 10){
+    console.log(num)
+    // num +=1;
+}
+
+
+// Even Check for While Loop uses
+
+var token1 = 0;
+while(token1 <= 20){
+    console.log(token1)
+    token1 += 2;
+}
+
+var token2 = 1;
+while(token2 <= 20){
+    console.log(token2)
+    token2 += 2;
+}
